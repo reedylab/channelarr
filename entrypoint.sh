b8a44edee949e13bd40d0b9e47db6979be43f50e
Reedy Lab <admin@reedylab.com>
2026-03-31 16:18:21 +0000

    Update entrypoint: gunicorn to uvicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@ set -e
 
 mkdir -p /app/logs /app/data /app/data/hls
 
-exec gunicorn -c web/gunicorn.config.py web.app:app
+exec uvicorn web.app:app --host 0.0.0.0 --port 5045 --workers 1 --timeout-keep-alive 75
